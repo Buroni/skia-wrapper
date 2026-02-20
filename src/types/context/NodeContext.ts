e46337d7ecee05b4f89642cb467d84ac602b7176
Jake Reid Browning <jbrowning@jigsawcreate.com>
2026-02-20 14:48:23 +0000

    type: fix NodeContext type error

--- src/types/context/NodeContext.ts
+++ src/types/context/NodeContext.ts
@@ -1,7 +1,8 @@
 import type { CanvasNode, CanvasNodePathData } from "../CanvasNode";
 import type { CanvasNodeStyle } from "../CanvasNodeStyle";
+import type { LabelOptions } from "../LabelOptions";
 
 export type NodeContext = {
     addFont: (name: string, url: string) => Promise<void>;
-    createNode: (pathData: CanvasNodePathData, nodeStyle: CanvasNodeStyle) => CanvasNode
+    createNode: (pathData: CanvasNodePathData, options: { nodeStyle?: CanvasNodeStyle, labelOptions?: LabelOptions }) => CanvasNode
 };
