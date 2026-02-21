2d427555ccc66567f1e929d6d6959f330e42e5d7
Jake Reid Browning <jbrowning@jigsawcreate.com>
2026-02-21 17:07:54 +0000

    feat: dragDrop listeners

--- src/dragDrop.ts
+++ src/dragDrop.ts
@@ -2,7 +2,7 @@ import { type CanvasNode } from "./types/CanvasNode";
 import { InteractionKeys, useInteractionManager } from "./interactionManager";
 import { type SkiaContext } from "./types/context/SkiaContext";
 
-export function useDragDrop(skiaContext: SkiaContext): void {
+export function useDragDrop(skiaContext: SkiaContext): any {
     const DRAG_DROP_KEY = "DRAG_DROP_KEY";
     const {
         setInteraction,
@@ -15,6 +15,32 @@ export function useDragDrop(skiaContext: SkiaContext): void {
     let dy = 0;
 
     setupEventListeners();
+
+    let dragStartFn: (e: MouseEvent) => void;
+    let draggingFn: (e: MouseEvent) => void;
+    let dragEndFn: (e: MouseEvent) => void;
+
+    let _isLastPointerMoveDrag = false;
+
+    function onDragStart(fn: (e: MouseEvent) => void): void {
+        dragStartFn = fn;
+    }
+
+    function onDragging(fn: (e: MouseEvent) => void): void {
+        draggingFn = fn;
+    }
+
+    function onDragEnd(fn: (e: MouseEvent) => void): void {
+        dragEndFn = fn;
+    }
+
+    function isDragging(): boolean {
+        return Boolean(dragging);
+    }
+
+    function isLastPointerMoveDrag() {
+        return _isLastPointerMoveDrag;
+    }
 
     function hitTest(): void {
         for (const node of skiaContext.nodes) {
@@ -30,7 +56,7 @@ export function useDragDrop(skiaContext: SkiaContext): void {
     function setupEventListeners() {
         const { canvasEl } = skiaContext;
 
-        canvasEl.addEventListener('pointerdown', async () => {
+        canvasEl.addEventListener('pointerdown', async (e: MouseEvent) => {
             if (hasInteraction()) {
                 return;
             }
@@ -39,21 +65,40 @@ export function useDragDrop(skiaContext: SkiaContext): void {
 
             if (dragging) {
                 setInteraction();
+                dragStartFn?.(e);
+
                 dx = skiaContext.mouse.worldX - dragging.pathData.cx;
                 dy = skiaContext.mouse.worldY - dragging.pathData.cy;
             }
         });
 
-        canvasEl.addEventListener('pointermove', () => {
+        canvasEl.addEventListener('pointermove', (e: MouseEvent) => {
+            console.log(_isLastPointerMoveDrag);
             if (dragging) {
+                _isLastPointerMoveDrag = true;
+
+                draggingFn?.(e);
+
                 dragging.pathData.cx = skiaContext.mouse.worldX - dx;
                 dragging.pathData.cy = skiaContext.mouse.worldY - dy;
+            } else {
+                _isLastPointerMoveDrag = false;
             }
         });
 
-        canvasEl.addEventListener('pointerup', () => {
+        canvasEl.addEventListener('pointerup', (e: MouseEvent) => {
+            dragEndFn?.(e);
+
             dragging = null;
             releaseInteraction();
         });
+    }
+
+    return {
+        onDragStart,
+        onDragging,
+        onDragEnd,
+        isDragging,
+        isLastPointerMoveDrag
     }
 }
